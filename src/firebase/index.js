e6d1842e6c5375eefc1ab015508e599452463ce7
Mamoru Fujinoki <mfujinoki@fujibiz.com>
2019-02-13 14:19:23 -0800

    First commit

---
+++ src/firebase/index.js
@@ -0,0 +1,11 @@
+import firebase from 'firebase';
+// Initialize Firebase
+const config = {
+    apiKey: "AIzaSyAgqM3n_zl3y9F_c6kERbt4DnfGFb3hHu0",
+    authDomain: "meal-prep-3c98a.firebaseapp.com",
+    databaseURL: "https://meal-prep-3c98a.firebaseio.com",
+    projectId: "meal-prep-3c98a",
+    storageBucket: "meal-prep-3c98a.appspot.com",
+    messagingSenderId: "1016807824127"
+};
+firebase.initializeApp(config);
